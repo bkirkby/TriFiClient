291a6c96ebe834543e73ec1ce2991fc017029c43
Kirkby <kirkby@c8bcc88e930c.ant.amazon.com>
2014-06-14 10:31:44 -0700

    Initial Commit

---
+++ TriFiClient/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TriFiClient
+//
+//  Created by Kirkby, Brian on 6/14/14.
+//  Copyright (c) 2014 Zapos IP Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
